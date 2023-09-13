a703bc40bfe26058b5abbf7b50cbaabd85bbfc0d
Jacob Holland <jacobfholland@gmail.com>
2023-09-13 19:55:38 -0400

    [FIX] Doc strings

--- app/utils/printable.py
+++ app/utils/printable.py
@@ -1,6 +1,23 @@
 class Printable:
+    """A mixin class for objects that can be represented as strings and have a printable representation.
+
+    This class provides default implementations for the `__str__` and `__repr__` methods, which return a string
+    representation of the object's class name and its attributes.
+    """
+
     def __repr__(self) -> str:
+        """Return a string representation of the object suitable for debugging.
+
+        Returns:
+            str: A string containing the class name and the object's attributes.
+        """
         return self.__str__()
 
     def __str__(self) -> str:
+        """Return a string representation of the object.
+
+        Returns:
+            str: A string containing the class name and the object's attributes.
+        """
+
         return f"{self.__class__.__name__}({vars(self)})"
--- config/app.py
+++ config/app.py
@@ -3,11 +3,29 @@ import os
 from dotenv import load_dotenv
 
 
-def strip(input):
+def strip(input: str) -> str:
+    """Strip the last two components from a file path.
+
+    Args:
+        input (str): Input file path.
+
+    Returns:
+        str: File path with the last two components removed.
+    """
+
     return "/".join(input.split("/")[:-2])
 
 
-def load_envs(directory):
+def load_envs(directory: str) -> None:
+    """Load environment variables from .env files found in the specified directory and its subdirectories.
+
+    Args:
+        directory (str): The directory where .env files should be searched and loaded from.
+
+    Returns:
+        None
+    """
+
     for root, _, files in os.walk(directory):
         for file in files:
             if ".env" in file:
@@ -22,6 +40,15 @@ load_envs(APP_DIR)
 
 
 class Config:
+    """Configuration class for the application.
+
+    Attributes:
+        APP_NAME (str): The name of the application obtained from the environment variables.
+        APP_DIR (str): The application's directory path.
+        LOG_LEVEL (str): The log level for the application obtained from the environment variables.
+        LOG_PATH (str): The path where log files should be stored obtained from the environment variables.
+    """
+
     APP_NAME = os.environ.get("APP_NAME")
     APP_DIR = APP_DIR
 
