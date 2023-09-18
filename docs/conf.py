9248b86a1f2bbe3e7e4dcd92ca52188ed4eeb78a
Jacob Holland <jacobfholland@gmail.com>
2023-09-17 21:56:18 -0400

    [FIX] RTD theme

--- docs/conf.py
+++ docs/conf.py
@@ -11,5 +11,5 @@ release = '0.0.1'
 extensions = ['sphinx.ext.autodoc']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'alabaster'
 html_static_path = ['_static']
