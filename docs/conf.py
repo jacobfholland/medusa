e7ea2ee61523e2e450540f93305b282d0546390f
Jacob Holland <jacobfholland@gmail.com>
2023-09-17 22:10:19 -0400

    [FIX] RTD config

--- docs/conf.py
+++ docs/conf.py
@@ -4,12 +4,12 @@ path = os.path.abspath("./..")
 sys.path.insert(0, path)
 
 
-project = 'Medusa'
-copyright = '2023, Jacob Holland'
-author = 'Jacob Holland'
-release = '0.0.1'
-extensions = ['sphinx.ext.autodoc']
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-html_theme = 'alabaster'
-html_static_path = ['_static']
+project = "Medusa"
+copyright = "2023, Jacob Holland"
+author = "Jacob Holland"
+release = "0.0.1"
+extensions = ["sphinx.ext.autodoc"]
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
