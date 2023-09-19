94bd329e2ba312438dc29923201c3f07ffc4e69a
Jacob Holland <jacobfholland@gmail.com>
2023-09-19 09:53:21 -0400

    [IMP] Sphinx

--- docs/sphinx/conf.py
+++ docs/sphinx/conf.py
@@ -13,3 +13,10 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_context = {
+    "display_github": True,
+    "github_user": "jacobfholland",
+    "github_repo": "medusa",
+    "github_version": "master",
+    "conf_py_path": f"/docs/sphinx/"
+}
