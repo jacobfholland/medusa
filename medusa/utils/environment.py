9c89709e338489a8dcff9a46340979bc92b05b80
Jacob Holland <jacobfholland@gmail.com>
2023-09-21 10:59:34 -0400

    [IMP] Environment docstrings

--- medusa/utils/environment.py
+++ medusa/utils/environment.py
@@ -9,24 +9,18 @@ def require_envs(config: object, envs: List[str]) -> Callable:
     """A decorator that checks if the specified environment variables are present.
 
     Args:
-        - `config` (object): The configuration object that should contain the environment variables.
+        - `config` (object): The configuration object that should contain the 
+            environment variables.
         - `envs` (List[str]): A list of environment variable names to check for.
+
+    Func Args:
+        - `args` (dict): Source function positional arguments.
+        - `kwargs` (dict): Source function key word arguments.
 
     Returns:
         `Callable`: The decorated function.
-    """
 
-    def check_envs(config: object, envs: List[str]) -> bool:
-        false_envs = [
-            env for env in envs
-            if not vars(config).get(env)
-        ]
-        if false_envs:
-            logger.error(
-                f"Missing required environment variables: {', '.join(false_envs)}"
-            )
-            sys.exit(1)
-        return True
+    """
 
     def decorator(func: Callable) -> Callable:
         @functools.wraps(func)
@@ -35,3 +29,31 @@ def require_envs(config: object, envs: List[str]) -> Callable:
                 return func(*args, **kwargs)
         return wrapper
     return decorator
+
+
+def check_envs(config: object, envs: List[str]) -> bool:
+    """Checks if the specified environment variables are present.
+
+    Args:
+        - `config` (object): The configuration object that should contain the 
+            environment variables.
+        - `envs` (List[str]): A list of environment variable names to check for.
+
+    Func Args:
+        - `args` (dict): Source function positional arguments.
+        - `kwargs` (dict): Source function key word arguments.
+
+    Returns:
+        `bool`: If the environment variable is present.
+    """
+
+    false_envs = [
+        env for env in envs
+        if not vars(config).get(env)
+    ]
+    if false_envs:
+        logger.error(
+            f"Missing required environment variables: {', '.join(false_envs)}"
+        )
+        sys.exit(1)
+    return True
